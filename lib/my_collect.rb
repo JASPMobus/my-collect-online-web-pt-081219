808339c747aa86796b175531d9e919bb65300736
James Mobus <mobusj@live.com>
2019-08-21 00:51:43 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,8 +2,8 @@ def my_collect(array)
   retarray = []
   counter = 0
   
-  while counter < array.length do |i|
-    retarray.push(yield(array[i]))
+  while counter < array.length do
+    retarray.push(yield(array[counter]))
   end
   
   retarray
